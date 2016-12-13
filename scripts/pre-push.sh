8e759e9d39b2fe678161bd3f0acf93582220e2ce
Kiyan Gauss <fourcels@gmail.com>
2016-12-13 08:57:01 +0800

    update style

---
+++ scripts/pre-push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#.git/hooks/pre-push
+
+if [ "$(git symbolic-ref HEAD 2>/dev/null)" == "refs/heads/master" ] ; then
+  echo "Checking if src changed."
+  # precompile assets if any have been updated
+  if git diff-index --name-only HEAD~1 | egrep '^src/' >/dev/null ; then
+    echo "Precompiling build"
+    gulp build --env production
+    echo "Builded production assets"
+    git add -A assets/
+    git commit -m "Updated assets for production"
+    echo "Committed new assets"
+  fi
+fi
