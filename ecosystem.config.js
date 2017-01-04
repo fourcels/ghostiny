a156096bc1ae780639cdeb85a118be6e107b151b
Kiyan Gauss <fourcels@gmail.com>
2017-01-04 15:51:12 +0800

    post-deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,3 @@
-var production =  {
-  user : "node",
-  host : "23.106.132.243",
-  ref  : "origin/master",
-  repo : "https://github.com/fourcels/ghostiny.git",
-  path : "~/ghostiny"
-}
-
 module.exports = {
   /**
    * Application configuration section
@@ -37,6 +29,13 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
   deploy : {
-    "prod" : production,
+    production : {
+      user : "node",
+      host : "23.106.132.243",
+      ref  : "origin/master",
+      repo : "https://github.com/fourcels/ghostiny.git",
+      path : "~/ghostiny",
+      "post-deploy" : "pm2 reload ghost"
+    }
   }
 }
