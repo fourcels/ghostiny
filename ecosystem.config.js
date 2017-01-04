148bf88e9266de2cd8a06f269f4ca65b1e6beeed
Kiyan Gauss <fourcels@gmail.com>
2017-01-04 13:14:08 +0800

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "API",
+      script    : "app.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    },
+
+    // Second application
+    {
+      name      : "WEB",
+      script    : "web.js"
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "node",
+      host : "23.106.132.243",
+      ref  : "origin/master",
+      repo : "https://github.com/fourcels/ghostiny.git",
+      path : "~/ghostiny",
+      "post-deploy" : "pm2 startOrReload ghost"
+    }
+  }
+}
