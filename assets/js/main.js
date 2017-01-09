c7a5b08faa78cde19023a72171295d8f7dd37dbc
Kiyan Gauss <fourcels@gmail.com>
2017-01-09 13:58:49 +0800

    Prism hightlight

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,6 @@ document.addEventListener("turbolinks:render", function() {
   NProgress.done();
   NProgress.remove();
 })
+document.addEventListener("turbolinks:load", function() {
+  Prism.highlightAll();
+})
